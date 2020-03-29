16d1e2dad90cd5c510c380a477c2be1e3949c2a4
Garrett Leising <gzleising@gmail.com>
2020-03-29 04:52:44 -0700

    removed some routes that are not being used

--- server/routes.py
+++ server/routes.py
@@ -1,10 +1,8 @@
 from flask import render_template, url_for, flash, redirect, request, abort, Blueprint, current_app
-from .constants import ip_address, overall_facebook_spending, overall_facebook_spending_chloro, dashboard
+from .constants import ip_address, dashboard
 
 router = Blueprint('router', __name__)
 
-overall_facebook_spending_addr = f"{ip_address}:{overall_facebook_spending}"
-overall_facebook_spending_chloro_addr = f"{ip_address}:{overall_facebook_spending_chloro}"
 dashboard_addr = f"{ip_address}:{dashboard}"
 
 @router.route("/", methods=["GET", "POST"])
@@ -14,10 +12,6 @@ def home():
 @router.route("/forecast_dashboard", methods=["GET", "POST"])
 def forecast_dashboard():
     return render_template('forecast_dashboard.html', title="GridCast - Forecast", iframesrc=dashboard_addr)
-
-@router.route("/national_report", methods=["GET", "POST"])
-def national_report():
-    return render_template('national_report.html', title="GridCast - National Report", iframesrc=overall_facebook_spending_addr, iframesrc_map=overall_facebook_spending_chloro_addr)
 
 @router.route("/static/num_accidents.geojson", methods=["GET", "POST"])
 def traffic_data():
