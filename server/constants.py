10783f9d4c3a9920c26515d4d05b65633173ea77
Garrett Leising <gzleising@gmail.com>
2020-03-28 20:36:13 -0700

    setup is generally done, just have to fix some of the html and server variables

---
+++ server/constants.py
@@ -0,0 +1,8 @@
+import os
+
+ip_address = "http://127.0.0.1"
+#ip_address = "http://transparencyproject.tech"
+
+overall_facebook_spending = 8050
+overall_facebook_spending_chloro = 8052
+dashboard = 8051
